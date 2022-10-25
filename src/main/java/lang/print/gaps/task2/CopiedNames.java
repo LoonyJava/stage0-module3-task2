be466a936032425fa07775a2297cf70109b9cc8e
LoonyJava <loony.java@gmail.com>
2022-10-25 12:38:17 +0500

    Task #2. Key Points

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,9 +3,9 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int secVariable = 2000;
+        int variableTwo = 2000;
         int value = 1000;
-        int thirdVariable = 4000;
+        int variableThree = 4000;
         int value2 = 3000;
         int variables = 5000;
         variables = 7000;
